e581b1e72f104ff0279bc5ce743e0d0bac4cb6d1
motsei-labs <224332645@tut4life.ac.za>
2024-08-01 22:48:25 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author USER
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyFirstFrame dd;
+        dd = new MyFirstFrame();
+        
+    }
+    
+}
